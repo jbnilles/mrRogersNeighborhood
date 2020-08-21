c0b5247ea54ce91bbc51a5ac20153665f2624dd3
jbnilles <jbnilles24@gmail.com>
2020-08-21 09:53:52 -0700

    add an add name button and input and correspondig js code for functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function createList (number) {
+function listWithNumber (number) {
   let result = [];
   for(let i = 0; i < number; i++) {
     if (i.toString().includes('3')) {
@@ -13,6 +13,36 @@ function createList (number) {
   }
   return result;
 }
+function isName(name) {
+  if(name === "") {
+    return false;
+  }
+  return true;
+}
+function listWithName (number, name) {
+  let result = [];
+  for(let i = 0; i < number; i++) {
+    if (i.toString().includes('3')) {
+      result.push("Won't you be my neighbor, " + name + "?");
+    } else if(i.toString().includes('2')) {
+      result.push('Boop!');
+    } else if (i.toString().includes('1')) {
+      result.push('Beep!');
+    } else {
+      result.push(i);
+    }
+  }
+  return result;
+}
+function createList (number, name) {
+  if(isName(name)) {
+    return listWithName(number, name);
+  } else {
+    return listWithNumber(number);
+  }
+
+}
+
 
 
 
@@ -20,17 +50,26 @@ $(document).ready(function () {
 
   $('#listForm').submit(function () {
     event.preventDefault();
-    let userInput = parseInt($('#listInput').val());
-    let result = createList(userInput);
-    $('#resultList').prepend('<li>' + userInput + ': <ul><li>' + result.join(', ') + '</li></ul></li>');
+    let userNumber = parseInt($('#listInput').val());
+    let userName = $('#nameInput').val();
+    let result = createList(userNumber, userName);
+    $('#resultList').prepend('<li>' + userNumber + ': <ul><li>' + result.join(', ') + '</li></ul></li>');
     $('#resultCard').show();
+    //alert($('#nameInput').val());
   });
   $('#reverseButton').click(function () {
     event.preventDefault();
-    let userInput = parseInt($('#listInput').val());
-    let result = createList(userInput);
-    $('#resultList').prepend('<li>' + userInput + ': <ul><li>' + result.reverse().join(', ') + '</li></ul></li>');
+    let userNumber = parseInt($('#listInput').val());
+    let userName = $('#nameInput').val();
+    let result = createList(userNumber, userName);
+    $('#resultList').prepend('<li>' + userNumber + ': <ul><li>' + result.reverse().join(', ') + '</li></ul></li>');
     $('#resultCard').show();
+  });
+  $('#nameP').click(function () {
+    $('#nameInput').val('');
+    $('#nameLabel').fadeToggle();
+    $('#nameInput').fadeToggle();
+
   });
 
 });
