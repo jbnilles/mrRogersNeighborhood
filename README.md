756d0f620a0c0db4e33e0de386b20ca2b8313041
jbnilles <jbnilles24@gmail.com>
2020-08-21 11:49:57 -0700

    Refactor code and edit the README file

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# _{Application Name}_
+# _Mr. Rogers Neighborhood_
 
-#### _{Brief description of application}, 8/21/20_
+#### _Project for week 3 indpendent project at Epicodus, 8/21/20_
 
 #### By _**Joseph Nilles**_
 
 ## Description
 
-_{This is a detailed description of your application. Its purpose and usage.  Give as much detail as needed to explain what the application does, and any other information you want users or other developers to have. }_
+_This program takes a user input of a number and it will display a string, that lists all the numbers up to and including the one entered and will search and replace and number that includes a 1,2 or a 3 and replace it with a beeb! for 1, a boob! for 2 and 'Won't you be my neighbor?' for 3;
 
 ### Specs
 | Spec | Input | Output |
@@ -20,28 +20,23 @@ _{This is a detailed description of your application. Its purpose and usage.  Gi
 
 ## Setup/Installation Requirements
 
-* _This is a great place_
-* _to list setup instructions_
-* _in a simple_
-* _easy-to-understand_
-* _format_
-
-_{Leave nothing to chance! You want it to be easy for potential users, employers and collaborators to run your app. Do I need to run a server? How should I set up my databases? Is there other code this app depends on?}_
+* _Clone this repository_
+* _run index.html in a browser_
 
 ## Known Bugs
 
-_{Are there issues that have not yet been resolved that you want to let users know you know?  Outline any issues that would impact use of your application.  Share any workarounds that are in place. }_
+_does not handle user input that is negative_
 
 ## Support and contact details
 
-_{Let people know what to do if they run into any issues or have questions, ideas or concerns.  Encourage them to contact you or make a contribution to the code.}_
+_ contact the auther at: jbnilles24@gmail.com_
 
 ## Technologies Used
 
-_{Tell me about the languages and tools you used to create this app. Assume that I know you probably used HTML and CSS. If you did something really cool using only HTML, point that out.}_
+_This program uses HTML, CSS, Bootstrap, jQuery, and JavaScript_
 
 ### License
 
 *This software is available with an MIT license*
 
-Copyright (c) 2016 **_{List of contributors or company name}_**
+Copyright (c) 2020 **_Joseph Nilles_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,18 @@
-function listWithNumber (number) {
-  let result = [];
-  for(let i = 0; i < number; i++) {
-    if (i.toString().includes('3')) {
-      result.push("Won't you be my neighbor?");
-    } else if(i.toString().includes('2')) {
-      result.push('Boop!');
-    } else if (i.toString().includes('1')) {
-      result.push('Beep!');
-    } else {
-      result.push(i);
-    }
-  }
-  return result;
-}
 function isName(name) {
   if(name === "") {
     return false;
   }
   return true;
 }
-function listWithName (number, name) {
+
+function makeList(number, name) {
   let result = [];
+  if(isName(name)) {
+    name = ', ' + name;
+  }
   for(let i = 0; i < number; i++) {
     if (i.toString().includes('3')) {
-      result.push("Won't you be my neighbor, " + name + "?");
+      result.push("Won't you be my neighbor" + name + "?");
     } else if(i.toString().includes('2')) {
       result.push('Boop!');
     } else if (i.toString().includes('1')) {
@@ -34,46 +23,35 @@ function listWithName (number, name) {
   }
   return result;
 }
-function createList (number, name) {
-  if(isName(name)) {
-    return listWithName(number, name);
-  } else {
-    return listWithNumber(number);
-  }
-
-}
-
-
-
 
 $(document).ready(function () {
 
   $('#listForm').submit(function () {
     event.preventDefault();
     let userNumber = parseInt($('#listInput').val());
-    let userName = $('#nameInput').val();
-    let result = createList(userNumber, userName);
+    let userName = $('#nameInput').val().trim();
+    let result = makeList(userNumber, userName);
     $('#resultList').prepend('<li>' + userNumber + ': <ul><li>' + result.join(', ') + '</li></ul></li>');
     $('#resultCard').show();
-    //alert($('#nameInput').val());
   });
+
   $('#reverseButton').click(function () {
     event.preventDefault();
     let userNumber = parseInt($('#listInput').val());
-    let userName = $('#nameInput').val();
-    let result = createList(userNumber, userName);
+    let userName = $('#nameInput').val().trim();
+    let result = makeList(userNumber, userName);
     $('#resultList').prepend('<li>' + userNumber + ': <ul><li>' + result.reverse().join(', ') + '</li></ul></li>');
     $('#resultCard').show();
   });
+
   $('#nameP').click(function () {
     $('#nameInput').val('');
-    $('#nameLabel').fadeToggle();
-    $('#nameInput').fadeToggle();
+    $('#nameLabel').toggle();
+    $('#nameInput').toggle();
 
   });
   $('#clearButton').click(function () {
     $('#resultList').text('');
   });
-
 
 });
